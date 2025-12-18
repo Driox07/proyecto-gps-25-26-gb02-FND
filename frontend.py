da622dd6bd8aeaa4dd427e743f80e9a59e8b7655
Driox07 <82775672+Driox07@users.noreply.github.com>
2025-12-18 18:12:47 +0100

    fix string port

--- frontend.py
+++ frontend.py
@@ -2,4 +2,4 @@ import uvicorn
 import os
 
 if __name__ == "__main__":
-    uvicorn.run("controller.oversound_controller:app", host=os.getenv("SERVER_IP", "127.0.0.1"), port=os.getenv("SERVER_PORT", "8000"), reload=True)
+    uvicorn.run("controller.oversound_controller:app", host=os.getenv("SERVER_IP", "127.0.0.1"), port=int(os.getenv("SERVER_PORT", 8000)), reload=True)
