54baca37a3f9b5a4fb91cc92cf24f4d60973ca8a
Driox07 <82775672+Driox07@users.noreply.github.com>
2025-12-18 12:26:37 +0100

    Uploaded Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM ubuntu:latest
+
+RUN apt-get update && apt upgrade -y && apt install python3-pip python3-venv -y && apt autoremove -y
+COPY . /fnd
+WORKDIR /fnd
+RUN python3 -m venv .venv
+RUN .venv/bin/pip install --no-cache-dir -r requirements.txt
+EXPOSE 8000
+CMD [".venv/bin/python", "frontend.py"]
--- controller/msvc_servers.py
+++ controller/msvc_servers.py
@@ -1,3 +1,4 @@
+import os
 """
 ES IMPORTANTE CONFIGURAR CORRECTAMENTE ESTAS DIRECCIONES, PARA ASEGURAR 
 EL CORRECTO FUNCIONAMIENTO DE LA PLATAFORMA.
@@ -6,9 +7,9 @@ TAMBIÉN ES IMPORTANTE ACCEDER A CADA SERVICIO DESDE LA MISMA IP QUE AQUÍ FIGUR
 PARA ASEGURAR EL CORRECTO FUNCIONAMIENTO DE LAS COOKIES DEL SITIO.
 """
 
-FND = 'http://localhost:8000' # Frontend
-SYU = 'http://localhost:8080' # Servicio de Usuarios y Sesiones
-TYA = 'http://localhost:8081' # Servicio de Temas y Autores
-TPP = 'http://localhost:8082' # Tienda y pasarela de pago
-PT  = 'http://localhost:8083' # Proveedor de tracks
-RYE = 'http://localhost:8084' # Recomendaciones y Estadísticas
+FND = os.getenv('HOST_FND', 'http://localhost:8000') # Frontend
+SYU = os.getenv('HOST_SYU', 'http://localhost:8080') # Servicio de Usuarios y Sesiones
+TYA = os.getenv('HOST_TYA', 'http://localhost:8081') # Servicio de Temas y Autores
+TPP = os.getenv('HOST_TPP', 'http://localhost:8082') # Tienda y pasarela de pago
+PT  = os.getenv('HOST_PT',  'http://localhost:8083') # Proveedor de tracks
+RYE = os.getenv('HOST_RYE', 'http://localhost:8084') # Recomendaciones y Estadísticas
